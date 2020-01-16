d7dee5b455432c898a9223731d3505eea54a3df4
Renan Muniz <fmrenan@outlook.com>
2020-01-15 23:41:11 -0300

    Initial commit

---
+++ mobile/src/components/SafeAreaView.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Platform, StatusBar, SafeAreaView } from 'react-native';
+
+export default props => (
+  <SafeAreaView style={styles.AndroidSafeArea} {...props} >
+    {props.children}
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+  }
+});
